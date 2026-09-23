b55b253a41b91d0345eea037028ff3ca91b44cbf
Yago Andrade <100331672+yagoandradelima@users.noreply.github.com>
2026-09-23 18:44:54 -0300

    coletando as corridas

--- collect.py
+++ collect.py
@@ -4,16 +4,22 @@ import pandas as pd
 pd.set_option('display.max_columns', None)
 
 # %%
-# Pegando os dados generalistas da corrida e carrega os dados
-session = fastf1.get_session(2021, 7, 'R')
-session.load()
 
-# %%
-# Exibe e salva os dados obtidos
-session.results
-session.results.to_parquet("data/2021_07_R.parquet")
+for i in range(1,50):
 
-# %%
-# Le os dados do ambiente local
-df = pd.read_parquet(r'data\2021_07_R.parquet')
-df
+    print(f"Coletando GP {i}...")
+
+    try:
+        # Pega os dados generalistas da corrida e carrega
+        session = fastf1.get_session(2021, i, 'R')
+    except ValueError as err:
+        print(err)
+        break
+    # Carregando apenas os resultados finais
+    session._load_drivers_results()
+
+    # Exibe e salva os dados obtidos
+    session.results
+    session.results.to_parquet(f"data/2021_{i}_R.parquet")
+
+    print(session.results)
