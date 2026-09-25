4437ec53111623828f7cec78f166d23a2dc90893
Yago Andrade <100331672+yagoandradelima@users.noreply.github.com>
2026-09-25 11:27:09 -0300

    Update collect.py
    Criando uma classe para a coleta de dados

--- collect.py
+++ collect.py
@@ -4,22 +4,52 @@ import pandas as pd
 pd.set_option('display.max_columns', None)
 
 # %%
+# Criando uma classe de coleta de dados
+class CollectResults:
 
-for i in range(1,50):
+    def __init__(self, years=[2021, 2022, 2023], modes=["R", "S"]):
+        self.years = years
+        self.modes = modes
 
-    print(f"Coletando GP {i}...")
+    def get_data(self, year, gp, mode) -> pd.DataFrame:
+        try:
+            session = fastf1.get_session(year, gp, mode)
 
-    try:
-        # Pega os dados generalistas da corrida e carrega
-        session = fastf1.get_session(2021, i, 'R')
-    except ValueError as err:
-        print(err)
-        break
-    # Carregando apenas os resultados finais
-    session._load_drivers_results()
+        except ValueError as err:
+            return pd.DataFrame()
 
-    # Exibe e salva os dados obtidos
-    session.results
-    session.results.to_parquet(f"data/2021_{i}_R.parquet")
+        session._load_drivers_results()
 
-    print(session.results)
+        df = session.results
+        df["mode"] = mode
+
+        return df
+
+    def save_data(self, df, year, gp, mode):
+        df.to_parquet(f"data/{year}_{gp:02}_{mode}.parquet")
+
+    def process(self, year, gp, mode):
+        df = self.get_data(year, gp, mode)
+
+        if df.empty:
+            return False
+
+        self.save_data(df, year, gp, mode)
+        return True
+
+    def process_year_modes(self, year):
+        for i in range(1, 50):
+            for mode in self.modes:
+                if not self.process(year, i, mode) and mode == "R":
+                    return
+
+    def process_years(self):
+        for year in self.years:
+            self.process_year_modes(year)
+
+# %%
+# Testando a classe de coleta de dados
+collect = CollectResults([2023, 2024, 2025], ["S", "R"])
+
+# %%
+collect.process_years()
